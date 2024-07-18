c613b8a1d37e33106aa9a4b077785ca12eb5f65e
irfnhanif <14.irfanh@gmail.com>
2024-07-18 14:36:29 +0700

    feat: Add many-to-many relationship between Group and User models

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
@@ -14,4 +15,14 @@ class Group extends Model
         'purpose',
         'description'
     ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
